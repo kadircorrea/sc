4539a76c921d289ba9154079e63526b19aee0813
kadircorrea <104391394+kadircorrea@users.noreply.github.com>
2025-10-03 11:16:15 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -6,27 +6,23 @@ REM ------------------------------
 REM Set the URL of the file to download
 SET FILE_URL=https://raw.githubusercontent.com/kadircorrea/sc/refs/heads/master/requirements.txt
 
-REM Set the output filename
-SET OUTPUT_FILE=requirements.txt
+REM Set the filename
+SET FILE_NAME=file.txt
 
-REM Define Downloads folder (default for IEUser)
-SET DOWNLOADS_FOLDER=C:\Users\sauce\Downloads
+REM Temporary folder (current working directory)
+SET TEMP_FOLDER=%CD%
+echo Downloading to temporary folder: %TEMP_FOLDER%
 
-REM Show target folder
-echo Downloading to folder: %DOWNLOADS_FOLDER%
-
-REM Download using PowerShell
-powershell -Command "Invoke-WebRequest -Uri '%FILE_URL%' -OutFile '%DOWNLOADS_FOLDER%\%FILE_NAME%'"
+REM Download the file using PowerShell
+powershell -Command "Invoke-WebRequest -Uri '%FILE_URL%' -OutFile '%TEMP_FOLDER%\%FILE_NAME%'"
 
 IF %ERRORLEVEL% EQU 0 (
-    echo ✅ File downloaded successfully: %DOWNLOADS_FOLDER%\%FILE_NAME%
+    echo ✅ File downloaded successfully: %TEMP_FOLDER%\%FILE_NAME%
 ) ELSE (
     echo ❌ Download failed
     exit /b 1
 )
 
-REM List files in Downloads folder
-echo -----------------------------
-echo Files in Downloads folder:
-dir "%DOWNLOADS_FOLDER%"
-echo -----------------------------
+REM Define Downloads folder
+SET DOWNLOADS_FOLDER=C:\Users\sauce\Downloads
+echo Copying file to Downloads folder: %DOWNLOADS_FOLDER%
