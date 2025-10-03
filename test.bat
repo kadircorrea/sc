e282641ff14e64062de2cf6e6807ff7d44279e12
Kadir Correa <kadir.correa@saucelabs.com>
2025-10-03 11:57:27 +0200

    Revert "Update test.bat"
    This reverts commit 48e0fd700bfbdd65024c9e957505f37f74c69c69.

--- test.bat
+++ test.bat
@@ -1,10 +1,10 @@
 @echo off
 REM ------------------------------
-REM Pre-run script to download a file on Windows, show progress and file size
+REM Pre-run script to download a file on Windows and check folder
 REM ------------------------------
 
 REM Set the URL of the file to download
-SET FILE_URL=https://examplefile.com/file-download/340
+SET FILE_URL=https://raw.githubusercontent.com/kadircorrea/sc/refs/heads/master/requirements.txt
 
 REM Set the filename
 SET FILE_NAME=requirements.txt
@@ -13,16 +13,11 @@ REM Temporary folder (current working directory)
 SET TEMP_FOLDER=%CD%
 echo Downloading to temporary folder: %TEMP_FOLDER%
 
-REM Download the file using PowerShell with progress
-powershell -Command ^
-    "$wc = New-Object System.Net.WebClient; ^
-    $wc.DownloadFile('%FILE_URL%', '%TEMP_FOLDER%\%FILE_NAME%'); ^
-    $size = (Get-Item '%TEMP_FOLDER%\%FILE_NAME%').Length; ^
-    Write-Host '✅ File downloaded successfully: %TEMP_FOLDER%\%FILE_NAME%'; ^
-    Write-Host '📦 File size: ' ([math]::Round($size/1KB,2)) ' KB'"
+REM Download the file using PowerShell
+powershell -Command "Invoke-WebRequest -Uri '%FILE_URL%' -OutFile '%TEMP_FOLDER%\%FILE_NAME%'"
 
 IF %ERRORLEVEL% EQU 0 (
-    echo Download step completed
+    echo ✅ File downloaded successfully: %TEMP_FOLDER%\%FILE_NAME%
 ) ELSE (
     echo ❌ Download failed
     exit /b 1
@@ -47,4 +42,3 @@ echo -----------------------------
 echo Files in Downloads folder:
 dir "%DOWNLOADS_FOLDER%"
 echo -----------------------------
-
